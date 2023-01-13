c21b21dd2ccf48ae319c52d208084e8002acd0f3
Shengsong Wu <shengsong.wu@compass.com>
2023-01-13 16:48:53 +0800

    add swagger documents

--- backend/src/form/form.controller.ts
+++ backend/src/form/form.controller.ts
@@ -7,15 +7,22 @@ import {
   Post,
   Put,
 } from "@nestjs/common";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { PageResult, success, UniformResponse } from "../common";
 import { FormDto } from "./form.dto";
 import { Form } from "./form.entity";
 import { FormService } from "./form.service";
 
+@ApiTags("forms")
 @Controller("form")
 export class FormController {
   constructor(private readonly formService: FormService) {}
 
+  @ApiOperation({ summary: "Get forms via pagination way" })
+  @ApiResponse({
+    status: 200,
+    type: Array<Form>,
+  })
   @Get("page/:page/:size")
   async list(
     @Param("page") page: number,
@@ -25,24 +32,41 @@ export class FormController {
     return success(result);
   }
 
+  @ApiOperation({ summary: "Get one form by its id" })
+  @ApiResponse({
+    status: 200,
+    type: Form,
+  })
   @Get(":id")
   async getOne(@Param("id") id: number): Promise<UniformResponse<Form>> {
     const result = await this.formService.findOne(id);
     return success(result);
   }
 
+  @ApiOperation({ summary: "Create a form with its questions" })
+  @ApiResponse({
+    status: 201,
+  })
   @Post()
   async create(@Body() createFormDto: FormDto): Promise<UniformResponse<void>> {
     await this.formService.create(createFormDto);
     return success();
   }
 
+  @ApiOperation({ summary: "Update a form with its questions" })
+  @ApiResponse({
+    status: 200,
+  })
   @Put()
   async edit(@Body() editFormDto: FormDto): Promise<UniformResponse<void>> {
     await this.formService.edit(editFormDto);
     return success();
   }
 
+  @ApiOperation({ summary: "Delete a form by its id" })
+  @ApiResponse({
+    status: 200,
+  })
   @Delete(":id")
   async delete(@Param("id") id: string): Promise<UniformResponse<void>> {
     await this.formService.remove(id);
--- backend/src/question/question.controller.ts
+++ backend/src/question/question.controller.ts
@@ -1,13 +1,19 @@
 import { Controller, Get, Param } from "@nestjs/common";
 import { success, UniformResponse } from "../common";
-
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { Question } from "./question.entity";
 import { QuestionService } from "./question.service";
 
+@ApiTags("questions")
 @Controller("question")
 export class QuestionController {
   constructor(private readonly questionService: QuestionService) {}
 
+  @ApiOperation({ summary: "Get form questions by form id" })
+  @ApiResponse({
+    status: 200,
+    type: Array<Question>,
+  })
   @Get("form/:formId")
   async getAllByForm(
     @Param("formId") formId: number
